db8c519fa0ebef3447d8f92ada72a7da78abe025
compsci <compsci@compsci>
2017-10-20 10:45:55 -0600

    working except crad

--- src/ceaser/BeginHere.java
+++ src/ceaser/BeginHere.java
@@ -1,10 +1,24 @@
 package ceaser;
 
+import java.util.Scanner;
+import java.util.concurrent.SynchronousQueue;
+
 public class BeginHere {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner kbd = new Scanner(System.in);
+				
+		CeaserCipher myCipher = new CeaserCipher();
+		
+		System.out.println("please enter the plain text to encode.");
+		String plainText = kbd.nextLine();
+		
+		System.out.println("Please enter a key:");
+		 int key = kbd.nextInt();
+		 System.out.println("Your key is: " + key);
+		 
+	String codedText =	myCipher.encode(plainText, key);
+	System.out.println("Your Secret message is: " + codedText);
 
-	}
-
+}
 }
