ed4423d3ec962e2bb9ccd9d69535905bb0412db2
compsci <compsci@compsci>
2017-10-24 09:50:56 -0600

    z wrap working

--- src/ceaser/BeginHere.java
+++ src/ceaser/BeginHere.java
@@ -1,24 +1,24 @@
 package ceaser;
 
 import java.util.Scanner;
-import java.util.concurrent.SynchronousQueue;
 
 public class BeginHere {
 
 	public static void main(String[] args) {
 		Scanner kbd = new Scanner(System.in);
-				
+		
 		CeaserCipher myCipher = new CeaserCipher();
 		
-		System.out.println("please enter the plain text to encode.");
+		System.out.println("Please enter the plain text to encode:");
 		String plainText = kbd.nextLine();
 		
-		System.out.println("Please enter a key:");
-		 int key = kbd.nextInt();
-		 System.out.println("Your key is: " + key);
-		 
-	String codedText =	myCipher.encode(plainText, key);
-	System.out.println("Your Secret message is: " + codedText);
+		System.out.println("Please enter the key:");
+		int key = kbd.nextInt();
+		System.out.println("Your key is: " + key);
+		
+		String codedText = myCipher.encode(plainText, key);
+		System.out.println("Your secret message is: " + codedText);
 
-}
+	}
+
 }
--- src/ceaser/CeaserCipher.java
+++ src/ceaser/CeaserCipher.java
@@ -1,35 +1,66 @@
+
 package ceaser;
 
-public class  CeaserCipher {
+public class CeaserCipher {
 	
 	private String alpha = "abcdefghijklmnopqrstuvwxyz";
 
 	public String encode(String plainText, int key) {
-		String secretText ="";
+		
+		String secretText="";
 		int index, keyAdded;
+		
 		System.out.println("test, alpha length = " + alpha.length());
 		System.out.println("test, first char is: " + alpha.charAt(0));
-		System.out.println("test, alpha length = " + alpha.charAt(alpha.length()-1));
+		System.out.println("test, last char is: " + alpha.charAt(alpha.length()-1));
 		
-		for (int i = 0; i <alpha.length()-1; i++) {
+		for (int i = 0; i < alpha.length(); i++) {
 			System.out.println(alpha.charAt(i));
 		}
-		
 		System.out.println("\n\n");
 		
-		for (int i=0; i< plainText.length(); i++) {
-			System.out.println(plainText.charAt(i));
+		for (int i=0; i < plainText.length()   ; i++) {
 			
+			System.out.println(plainText.charAt(i));
 		}
-		for(int i=0; i<plainText.length();i++) {
+		
+		/*
+		 * each char that I read from plainText I find the index in alpha
+		 * for example, read A look up index for A get 0
+		 *              read M look up index for M get 12
+		 * then, add key to that index, so A index 0 becomes 0+key,
+		 * and 0+key becomes my coded char
+		 * let's say key = 1, then A index 0, 0 + 1 = 1, charAt 1 is B
+		 * and that is my encoding
+		 */
+		
+		for (int i=0; i<plainText.length();i++) {
 			
 			index = alpha.indexOf(plainText.charAt(i));
 			//System.out.println("index = " + index);
+			
+			/** Doug's Method **/
+			keyAdded = (index + key) % alpha.length();
+			
+			/** Will's Method
 			keyAdded = index + key;
 			
+			
+			
+			
+			if (keyAdded >= alpha.length()) {
+				keyAdded-=alpha.length();
+			}
+			**/
 			System.out.println("encoded is " + alpha.charAt(keyAdded));
+			
+			
 		}
 		
+		
+		
 		return secretText;
+		
 	}
+
 }
