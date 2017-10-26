96b03924b70af8546445c8c6dacb766b86dc6100
compsci <compsci@compsci>
2017-10-26 10:39:13 -0600

    Another Commit of oct 27 17

--- src/ceaser/BeginHere.java
+++ src/ceaser/BeginHere.java
@@ -2,12 +2,17 @@ package ceaser;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class BeginHere {
 
 	public static void main(String[] args) {
+		String msg = "Would you like to encode a plain text message?";
+		int confirm = JOptionPane.showConfirmDialog(null, msg);
 		Scanner kbd = new Scanner(System.in);
 		
 		CeaserCipher myCipher = new CeaserCipher();
+		
 		
 		System.out.println("Please enter the plain text to encode:");
 		String plainText = kbd.nextLine().toLowerCase();
@@ -18,7 +23,6 @@ public class BeginHere {
 		
 		String codedText = myCipher.encode(plainText, key);
 		System.out.println("Your secret message is: " + codedText);
-		
 
 	}
 
